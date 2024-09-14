58363cbad680cf293a3331fb5210791b8a04161b
ibrahim1234566 <hemaazz123@gmail.com>
2024-09-14 03:52:00 +0300

    Add project files.

---
+++ Company.Data/Contexts/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,41 @@
+﻿using Company.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Contexts.Configurations
+{
+    public class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.ToTable("Departments");
+
+
+            builder.HasKey(d => d.Id);
+
+            builder.Property(d => d.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(d => d.Code)
+                   .IsRequired()
+                   .HasMaxLength(10); 
+
+            builder.Property(d => d.IsDeleted)
+                   .IsRequired();
+
+            builder.Property(d => d.CreatedAt)
+                   .HasDefaultValueSql("GETDATE()");
+
+            builder.HasMany(d => d.Employees)
+                   .WithOne(e => e.Department)
+                   .HasForeignKey(e => e.DepartmentId)
+                   .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
---
+++ Company.Data/Contexts/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,58 @@
+﻿using Company.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Contexts.Configurations
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+
+            builder.ToTable("Employees");
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Name)
+                   .IsRequired()
+                   .HasMaxLength(100); 
+
+            builder.Property(e => e.Age)
+                   .IsRequired();
+
+            builder.Property(e => e.Address)
+                   .HasMaxLength(200); 
+
+            builder.Property(e => e.Salary)
+                   .IsRequired()
+                   .HasColumnType("decimal(18,2)");
+
+            builder.Property(e => e.Email)
+                   .HasMaxLength(150);
+
+            builder.Property(e => e.PhoneNumber)
+                   .HasMaxLength(15);
+
+            builder.Property(e => e.HiringDate)
+                   .IsRequired();
+
+            builder.Property(e => e.ImgeUrl)
+                   .HasMaxLength(250); 
+
+            builder.Property(e => e.IsDeleted)
+                   .IsRequired();
+
+            builder.Property(e => e.CreatedAt)
+                   .HasDefaultValueSql("GETDATE()");
+
+            builder.HasOne(e => e.Department)
+                   .WithMany(d => d.Employees)
+                   .HasForeignKey(e => e.DepartmentId)
+                   .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
