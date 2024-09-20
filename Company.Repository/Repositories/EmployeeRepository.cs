2b32a10ab7c63a4aa2f9067c8e7f7859955f3124
ibrahim1234566 <hemaazz123@gmail.com>
2024-09-20 17:12:01 +0300

    Repository Pattern_part_2

--- Company.Repository/Repositories/EmployeeRepository.cs
+++ Company.Repository/Repositories/EmployeeRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Company.Data.Contexts;
+using Company.Data.Models;
+using Company.Repository.interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,32 @@ using System.Threading.Tasks;
 
 namespace Company.Repository.Repositories
 {
-    internal class EmployeeRepository
+    public class EmployeeRepository : IEmployeeRepository
     {
+        private readonly CompanyDbContext _context;
+
+        public EmployeeRepository(CompanyDbContext context)
+        {
+           _context = context;
+        }
+        public void Add(Employee employee)=>_context.Add(employee);
+      
+
+        public void Delete(Employee employee) => _context.Remove(employee); 
+
+
+
+        public IEnumerable<Employee> GetAll() => _context.Employees.ToList();
+
+
+
+        public Employee GetById(int id) => _context.Employees.FirstOrDefault(x=>x.Id==id);
+
+
+
+
+        public void Update(Employee employee) => _context.Update(employee);
+
+
     }
 }
