6c2b570ba286ad6878c0587e4fcb830972fa9523
ibrahim1234566 <hemaazz123@gmail.com>
2024-09-25 03:53:15 +0300

    Employee service - Index Action & View -Partial Views

---
+++ Company.Service/Services/EmployeeService.cs
@@ -0,0 +1,64 @@
+﻿using Company.Data.Models;
+using Company.Repository.interfaces;
+using Company.Repository.Repositories;
+using Company.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Service.Services
+{
+    public class EmployeeService:IEmployeeService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EmployeeService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Add(Employee employee)
+        {
+            _unitOfWork.employeeRepository.Add(employee);
+            _unitOfWork.Complete();
+        }
+
+        public void Delete(Employee employee)
+        {
+            _unitOfWork.employeeRepository.Delete(employee);
+            _unitOfWork.Complete();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            var emp = _unitOfWork.employeeRepository.GetAll();
+            return emp;
+        }
+
+        public Employee GetById(int? id)
+        {
+            if (id is null)
+            {
+                return null;
+            }
+            var emp = _unitOfWork.employeeRepository.GetById(id.Value);
+            if (emp is null)
+            {
+
+                return null;
+            }
+            return emp;
+        }
+
+        public IEnumerable<Employee> GetEmployeeByName(string name)=>_unitOfWork.employeeRepository.GetEmployeeByName(name);
+       
+
+        public void Update(Employee employee)
+        {
+            _unitOfWork.employeeRepository.Update(employee);
+            _unitOfWork.Complete();
+        }
+    }
+}
--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -19,9 +19,10 @@ namespace Company.Web
             builder.Services.AddDbContext<CompanyDbContext>(op =>
     op.UseSqlServer(builder.Configuration.GetConnectionString("myconn")));
 
-           // builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-          //  builder.Services.AddScoped<IDepartmetRepository, DepartmetRepository>();
+            // builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            //  builder.Services.AddScoped<IDepartmetRepository, DepartmetRepository>()
             builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+            builder.Services.AddScoped< IEmployeeService,EmployeeService> ();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
          //   builder.Services.AddScoped<IGenericRepository<Employee>, GenericRepository<Employee>>();
