4722ea7cdb42d3cebc3ed9c9b24454c28e4c47cc
ibrahim1234566 <hemaazz123@gmail.com>
2024-09-25 05:43:43 +0300

    Employee Create (Action & View) - ViewBag-ViewData-TempData

--- Company.Web/Controllers/EmployeeController.cs
+++ Company.Web/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Company.Service.Interfaces;
+﻿using Company.Data.Models;
+using Company.Service.Interfaces;
+using Company.Service.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Web.Controllers
@@ -6,10 +8,12 @@ namespace Company.Web.Controllers
     public class EmployeeController : Controller
     {
         private readonly IEmployeeService _employeeService;
+        private readonly IDepartmentService _departmentService;
 
-        public EmployeeController(IEmployeeService employeeService) 
+        public EmployeeController(IEmployeeService employeeService,IDepartmentService departmentService) 
         {
            _employeeService = employeeService;
+            _departmentService = departmentService;
         }
         [HttpGet]
         public IActionResult Index(string searchInp)
@@ -23,8 +27,38 @@ namespace Company.Web.Controllers
             else
             {
                 var emp = _employeeService.GetEmployeeByName(searchInp);
+                //ViewBag.Message = "this is message from ViewBag";          
                 return View(emp);
             }
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Departments = _departmentService.GetAll();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _employeeService.Add(employee);
+                    return RedirectToAction(nameof(Index));
+
+                }
+                ModelState.AddModelError("EmployeeError", "ValidationErrors");
+                return View(employee);
+
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("EmployeeError", ex.Message);
+                return View(employee);
+            }
+
         }
     }
 }
