45b4c35598e2db1a3db7dfb9e1993651e81d4932
ibrahim1234566 <hemaazz123@gmail.com>
2024-10-04 06:38:31 +0300

    Account Controller (SignIn)

--- Company.Web/Controllers/AccountController.cs
+++ Company.Web/Controllers/AccountController.cs
@@ -8,10 +8,12 @@ namespace Company.Web.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         [HttpGet]
         public IActionResult SignUp()
@@ -48,6 +50,42 @@ namespace Company.Web.Controllers
             }
             return View(input);
         }
+        [HttpGet]
 
+        public IActionResult SignIn()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task <IActionResult> SignIn(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if ( user is not null)
+                {
+                    var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, lockoutOnFailure: false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index","Home");
+
+                    }
+
+                    ModelState.AddModelError("","Login Failed");
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task <IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(SignIn));
+        }
     }
 }
