790c0b7dfcb15f5818b9dd1551369a0d71c9adc0
ibrahim1234566 <hemaazz123@gmail.com>
2024-10-05 04:29:50 +0300

    Account Controller (ForgetPassword)

--- Company.Web/Controllers/AccountController.cs
+++ Company.Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Company.Data.Models;
+using Company.Service.Helper;
 using Company.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -86,6 +87,34 @@ namespace Company.Web.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction(nameof(SignIn));
+        }
+
+        public async Task <IActionResult> ForgetPassword(ForgetPasswordViewModel input) 
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(input.Email);
+                if (user is not null)
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    var url = Url.Action("ResetPassword", "Account", new { Email = input.Email, Token = token }, Request.Scheme);
+                    var email = new Email
+                    {
+                        Body = url,
+                        Subject = "Reset Password",
+                        To = input.Email
+                    };
+                    EmailSettings.SendEmail(email);
+                    return RedirectToAction(nameof(CheckYourInbox));
+
+
+                }
+            }
+            return View(input);
+        }
+        public IActionResult CheckYourInbox()
+        {
+            return View();
         }
     }
 }
